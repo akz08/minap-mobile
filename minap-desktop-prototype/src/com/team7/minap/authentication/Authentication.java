51867b91d27d88bb60b97ae01a9954d5420aa3e1
Noodle-Soup <weike3721@gmail.com>
2013-11-18 12:47:40 +0000

    12:47 18/11/2013

---
+++ minap-desktop-prototype/src/com/team7/minap/authentication/Authentication.java
@@ -0,0 +1,22 @@
+package com.team7.minap.authentication;
+
+import com.team7.minap.network.Network;
+
+public class Authentication {
+	private String userName;
+	private String passWord;
+	private Network net;
+	
+	private static void setUser() {
+		
+	}
+	
+	private static void setPass() {
+		
+	}
+	
+	private static void authenticate(Network net, String user, String pass){
+		
+	}
+}
+
