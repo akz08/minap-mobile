d264885dda4486be860c92f759a2bd600c0c866f
Amer Kamil Zainal <amer.abidin.13@ucl.ac.uk>
2013-11-19 16:15:31 +0000

    Fleshed out authentication class

--- minap-desktop-prototype/src/com/team7/minap/authentication/Authentication.java
+++ minap-desktop-prototype/src/com/team7/minap/authentication/Authentication.java
@@ -1,3 +1,9 @@
+/**
+ * Main authentication class. Only called when initially logging in,
+ * or when network connection lost. For security reasons, we do not store user
+ * authentication details. 
+ */
+
 package com.team7.minap.authentication;
 
 import com.team7.minap.network.Network;
@@ -6,6 +12,11 @@ public class Authentication {
 	private static String userName;
 	private static String passWord;
 	private static Network net;
+	private static boolean isAuthenticated = false;
+	
+	private Authentication(Network n) {
+		net = n;
+	}
 	
 	private Authentication(String user, String pass, Network n) {
 		userName = user;
@@ -17,8 +28,19 @@ public class Authentication {
 		passWord = pass;
 	}
 	
+	/**
+	 * Method to call server to perform authentication.
+	 * 
+	 * @param net Network object to call
+	 * @param user Username of user
+	 * @param pass Password of user
+	 */
 	private static void authenticate(Network net, String user, String pass){
 		
+	}
+	
+	private static boolean isAuthenticated() {
+		return isAuthenticated;
 	}
 	
 	private static void signOut() {
