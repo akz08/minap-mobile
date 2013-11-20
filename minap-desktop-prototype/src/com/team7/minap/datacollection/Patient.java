f06930bdbb8ebb3a560be5decfe8f2405d9313e1
daviducl <vo2david@hotmail.com>
2013-11-20 17:21:24 +0000

    Added Patient class. Lots of fields, lots of options. Validation class slightly modified

---
+++ minap-desktop-prototype/src/com/team7/minap/datacollection/Patient.java
@@ -0,0 +1,32 @@
+package com.team7.minap.datacollection;
+
+import java.util.Date;
+
+public class Patient {
+	private static String caseNumber;
+	private static String surname, forename, nhsNumber, postCode, gpCode, ambulanceJobNumber, interventionalCode, 
+	referringHospital;
+	private static Date dob, symptomOnset, callHelp, firstArrival, ambulanceArrival, hospitalArrival, 
+	reperfusionTreatment, cardiacArrest, nonInterventionArrival, insulinStart, dischargeDate, 
+	investigationReferral, daycaseTransfer, angio, localIntervention, dateReturnReferringHospital;
+	private static double serumCholesterol, bpSystolic, heartRate, serumGlucose, height, weight, 
+	creatinine, haemoglobin, peakTroponin;
+	/**
+	 * Start of long code fields
+	 */
+	private static String gender, adminStatus, ethnicity, initialDiagnosis, ecgTreatment, aspirinGiven,
+	prevAMI, prevAngina, hypertension, hyperCholes, perVascularDisease, cerebroVascular, asthmaCOPD, 
+	renalFailure, heartFailure, cardiacRaised, smoking, diabetes, prevPCI, prevCABG, admitConsultant, 
+	ecgPerformed, betaBlocker, aceiARBUse, statin, leftVEF, familyCHD, cardioCare, infarctionSite, ecgqrsDuration, 
+	thienopyridineUse, admissionMethod, locationSTEMI, killip, stressEcho, iReperfusionNotGiven, treatmentDelay, 
+	iReperfusionGiven, arrestLocation, presentingRhythm, arrestOutcome, admissionWard,unfractionedHeparin, 
+	lmwHeparin, thienopyridineInhibitor, ivAgent, ivBetaBlocker, calciumBlocker, 
+	ivNitrate, oralNitrate, potassiumModulator, warfarin, aceiARB, thiazideDiuretic, loopDiuretic, thromboDrug, 
+	troponinAssay, fondaparinux, initialReperfusion, hyperglycemiaManagement, diabeticTherapyDischarge, 
+	oralBetaBlocker, aldosAntagonist, bivalirudin, nonInterventionAssessment, interventionAssessment, 
+	intendedProcedure, procedurePerformed, noAngio, noIntervention, dischargeDiagnosis, bleedingComplication, 
+	deathInHospital, dischargeBetaBlocker, dischageACEARB, dischargeStatin, dischageAspirin, cardiacRehab, 
+	exerciseTest, echoCardio, radioNuclideStudy, coronaryAngio, coronaryIntervention, dischargeDestination, 
+	followedUpBy, reInfarction, dischargeThienopyridine, dischargeAldosterone, procedurePerformedAt, angioDelay,
+	dischargeTicagrelor, highRiskSTEMI, smokingCessation, dietaryAdvice;    
+}
--- minap-desktop-prototype/src/com/team7/minap/validation/Validation.java
+++ minap-desktop-prototype/src/com/team7/minap/validation/Validation.java
@@ -1,5 +1,7 @@
 package com.team7.minap.validation;
 
+import java.util.Date;
+
 public class Validation {
 	
 	private static boolean isValid() {
@@ -16,5 +18,21 @@ public class Validation {
 	
 	private static void setValue() {
 		// must call isValid
+	}
+	
+	private static boolean dobCheck(Date dob) {
+		/**
+		 *  checks date for birthdate valid range
+		 *  patient must be between 20 and 120 years of age  
+		 */
+		return false;
+	}
+	
+	private static void rangeCheck() {
+		
+	}
+	
+	private static void typeCheck() {
+		
 	}
 }
