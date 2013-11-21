d47ff735e785328acec234cfe63570fac58761f5
Noodle-Soup <weike3721@gmail.com>
2013-11-21 19:00:50 +0000

    fleshed out patient class getters, setters and validation "else" cases as well as a bmi calculation prototype

--- minap-desktop-prototype/src/com/team7/minap/datacollection/Patient.java
+++ minap-desktop-prototype/src/com/team7/minap/datacollection/Patient.java
@@ -1,3 +1,12 @@
+/**
+ * The Patient class captures all 130 fields from the web application as they relate to one particular patient.
+ * In theory, caseNumber should be a unique value
+ * BMI and Grace Score are calculated values from something in validation package.
+ * 
+ * Java representation of Patient data.
+ * It is a singleton, there can only be one Patient class.
+ */
+
 package com.team7.minap.datacollection;
 
 import java.util.Date;
@@ -11,6 +20,9 @@ public class Patient {
 	investigationReferral, daycaseTransfer, angio, localIntervention, dateReturnReferringHospital;
 	private static double serumCholesterol, bpSystolic, heartRate, serumGlucose, height, weight, 
 	creatinine, haemoglobin, peakTroponin;
+	
+	// temporary constants
+	private static double CHOLES = 100;
 	/**
 	 * Start of long code fields
 	 */
@@ -28,5 +40,64 @@ public class Patient {
 	deathInHospital, dischargeBetaBlocker, dischageACEARB, dischargeStatin, dischageAspirin, cardiacRehab, 
 	exerciseTest, echoCardio, radioNuclideStudy, coronaryAngio, coronaryIntervention, dischargeDestination, 
 	followedUpBy, reInfarction, dischargeThienopyridine, dischargeAldosterone, procedurePerformedAt, angioDelay,
-	dischargeTicagrelor, highRiskSTEMI, smokingCessation, dietaryAdvice;    
+	dischargeTicagrelor, highRiskSTEMI, smokingCessation, dietaryAdvice; 
+	
+	private static double bmiScore; // double might be int CHECK
+	private static int graceScore;
+	
+	// Sample setters
+	
+	/**
+	 * setSurname takes in a String that is then set to Patient's surname variable
+	 * @param lastName
+	 */
+	public static void setSurname(String lastName) {
+		//check type first.
+		surname = lastName;
+	}
+	
+	public static void setSerumCholesterol(double cholesterol) {
+		//check type first.
+		// validate input first then set to variable
+		if (cholesterol > CHOLES)
+			serumCholesterol = cholesterol;
+		// else
+			//do validation.rangecheck.
+	}
+	
+	public static void setDOB(Date date) {
+		//check type first.
+		/*
+		 * if date taken in is between 20 and 120 years prior to Date.now()
+		 * set dob to date
+		 * else 
+		 * call Validation.dobCheck.
+		 */
+	}
+	
+	//Sample getters
+	
+	public static String getSurname() {
+		//getting surname
+		return surname;
+	}
+	
+	public static double getSerumCholesterol(){
+		//getting SerumCholesterol
+		return serumCholesterol;
+	}
+	
+	public static Date getDOB(){
+		//getting DOB
+		return dob;
+	}
+	
+	// calculated methods
+	public static void calcBMI() {
+		// assume calculators are valid
+		// once last calculator is entered, bmiScore should be pushed to appropriate field
+		// if (calculators are filled or not empty		
+		bmiScore = height / weight; // <- temporary placeholder calculation
+		// set bmi field to bmiScore
+	}
 }
--- minap-desktop-prototype/src/com/team7/minap/validation/Validation.java
+++ minap-desktop-prototype/src/com/team7/minap/validation/Validation.java
@@ -20,19 +20,31 @@ public class Validation {
 		// must call isValid
 	}
 	
-	private static boolean dobCheck(Date dob) {
+	private static boolean dateCheck(Date date) {
 		/**
-		 *  checks date for birthdate valid range
-		 *  patient must be between 20 and 120 years of age  
+		 * If it is wrong, do the following thing.
 		 */
+		System.out.println("DOB is invalid.");
+		//set the dob field to blank. And try again.
 		return false;
 	}
 	
-	private static void rangeCheck() {
-		
+	private static boolean rangeCheck() {
+		/** 
+		 * If it is wrong, do the following thing.
+		 */
+		System.out.println("Entry is not within range.");
+		//set the field to blank.
+		return false;
 	}
 	
-	private static void typeCheck() {
-		
+	private static boolean typeCheck() {
+		/** 
+		 * If it is wrong, do the following thing.
+		 */
+		// if type taken in is not what is expected
+		System.out.println("Invalid type");
+		// set field to blank
+		return false;
 	}
 }
