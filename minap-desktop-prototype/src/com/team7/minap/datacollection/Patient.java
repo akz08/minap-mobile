bfe07eee5a413b37b68097cd903736f2096cee52
Noodle-Soup <weike3721@gmail.com>
2013-11-27 16:15:09 +0000

    page 3,4,5 options from string to byte

--- minap-desktop-prototype/src/com/team7/minap/datacollection/Patient.java
+++ minap-desktop-prototype/src/com/team7/minap/datacollection/Patient.java
@@ -8,6 +8,7 @@
  */
 
 package com.team7.minap.datacollection;
+import com.team7.minap.utils.Algorithms;
 
 import java.util.Date;
 import java.lang.Double;
@@ -35,7 +36,7 @@ public class Patient {
 	/* Page 3: Initial reperfusion */
 	private static String invertentionalCentreCode;//no field no. given
 	// long / short code fields
-	private static String initialReperfusion,/*3.39, options*/ reperfusionNotGiven, /*3.08, options*/ ecgDetermineTreatment,/*2.03, options*/ locationAtSTEMI,/*2.40, options*/
+	private static byte initialReperfusion,/*3.39, options*/ reperfusionNotGiven, /*3.08, options*/ ecgDetermineTreatment,/*2.03, options*/ locationAtSTEMI,/*2.40, options*/
 	ecgQRSComplex, /*2.37, options*/infarctionSite;/*2.36, options*/
 	
 	//setter start here!
@@ -43,27 +44,27 @@ public class Patient {
 		iCentreCode = invertentionalCentreCode;
 	}
 	
-	public static void setInitialReperfusion (String iReperfusion){//3.39, options, Initial reperfusion treatment
+	public static void setInitialReperfusion (byte iReperfusion){//3.39, options, Initial reperfusion treatment
 		iReperfusion = initialReperfusion;
 	}
 	
-	public static void setReperfusionNotGiven (String rNotGiven){//3.08,options,Reason reperfusion treatment not given
+	public static void setReperfusionNotGiven (byte rNotGiven){//3.08,options,Reason reperfusion treatment not given
 		rNotGiven = reperfusionNotGiven;
 	}
 	
-	public static void setEcgDetermineTreatment (String eDetermineTreatment){//2.03,options,ECG determining treatment
+	public static void setEcgDetermineTreatment (byte eDetermineTreatment){//2.03,options,ECG determining treatment
 		eDetermineTreatment = ecgDetermineTreatment;
 	}
 	
-	public static void setLocationAtSTEMI (String lAtSTEMI){//2.40,options,Patient location at time of STEMI
+	public static void setLocationAtSTEMI (byte lAtSTEMI){//2.40,options,Patient location at time of STEMI
 		lAtSTEMI = locationAtSTEMI;
 	}
 	
-	public static void setEcgQRSComplex (String eQRSComplex){//2.37,options,ECG QRS complex duration
+	public static void setEcgQRSComplex (byte eQRSComplex){//2.37,options,ECG QRS complex duration
 		eQRSComplex = ecgQRSComplex;
 	}
 	
-	public static void setInfarctionSite (String iSite) {//2.36,options,Site of infarction
+	public static void setInfarctionSite (byte iSite) {//2.36,options,Site of infarction
 		iSite = infarctionSite;
 	}
 	//setter end here!
@@ -73,7 +74,7 @@ public class Patient {
 	private static Date referralDate, /*4.15 date*/angioDate,/*4.18 date*/ localInterventionDate, /*4.19 date*/daycaseTransferDate,/*4.17 date*/ angioReferHospitalReturnDate;/*4.26 date*/
 	private static String angioCentreCode;/*no field no. provided*/
 	// long / short code fields
-	private static String angioDelay,/*4.30 options*/ coronaryIntervention;/*4.14 options*/
+	private static byte angioDelay,/*4.30 options*/ coronaryIntervention;/*4.14 options*/
 	 
 	//setter start here!
 	public static void setAngioPerformed (boolean aPerformed){//Was angio performed during the admission? Y/N
@@ -109,21 +110,54 @@ public class Patient {
 		aCentreCode = angioCentreCode;
 	}
 	
-	public static void setAngioDelay (String aDelay){//4.30, options, Delay to performance of angiogram 
+	public static void setAngioDelay (byte aDelay){//4.30, options, Delay to performance of angiogram 
 		aDelay = angioDelay;
 	}
 	
-	public static void setCoronaryIntervention (String cIntervention){//4.14,options,Coronary intervention
+	public static void setCoronaryIntervention (byte cIntervention){//4.14,options,Coronary intervention
 		cIntervention = coronaryIntervention;
 	}
 	//setter end here!
 	
 	/* Page 5: Examinations */
-	private static double systolicBP, heartRate, height, weight;
+	private static double systolicBP,/*2.20, n3 */ heartRate, /*2.21, n3 */height,/*2.29, n3 */ weight;/*2.30, n3.1 */
 	// calculated fields
 	private static double BMI, GRACE;
 	// long / short code fields
-	private static String killipClass;
+	private static byte killipClass;/*2.41, options */
+	
+	//setters start here!
+	public static void setSystolicBP (double sBP){//2.20, n3, systolic blood pressure
+		sBP = systolicBP;
+	}
+	
+	public static void setHeartRate (double hRate){//2.21, n3,Heart rate
+		hRate = heartRate;
+	}
+	
+	public static void setHeight (double h){//2.29, n3, Height in cms to derive BMI.
+		h = height;
+	}
+	
+	public static void setWeight(double w){//2.30, n3, Weight in cms to derive BMI.
+		w = weight;
+	}
+	
+	public static void setKillipClass (byte kClass){//2.41, options, Killip class 
+		kClass = killipClass;
+	}
+	//setters end here!
+	
+	//getters start here!
+	public static void getBMI (double h,double w){
+		BMI = Algorithms.calculateBMI(h,w);
+	}
+	
+//	public static void getGRACE (){
+//		
+//	}
+	
+	//getters end here!
 	
 	/* Page 6: Medical History */
 	// long / short code fields
