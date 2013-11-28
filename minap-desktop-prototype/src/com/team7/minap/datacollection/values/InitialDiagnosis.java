989031d6be0848498428dd897578d07b4a2ffe00
david <david@10.111.121.138>
2013-11-28 15:43:29 +0000

    InitialDiagnosis as switch template

---
+++ minap-desktop-prototype/src/com/team7/minap/datacollection/values/InitialDiagnosis.java
@@ -0,0 +1,47 @@
+/** Field 2.01 - Initial Diagnosis
+	 * Takes in a single number (as a byte) that represents the patient's initial diagnosis.
+	 * This field's valid options are 
+	 * 1 - Definite Myocardial Infarction
+	 * 3 - Acute Coronary Syndrome
+	 * 4 - Chest pain? cause
+	 * 5 - Other Initial Diagnosis.
+	 * If option 3 (Acute Coronary Syndrome) is chosen, field 4.32 (High Risk nSTEMI) becomes visible
+	 * @param iniDiagnosis
+	 * @author david  
+	 */
+
+package com.team7.minap.datacollection.values;
+
+public class InitialDiagnosis extends Value {
+
+	private static byte initialDiagnosis;
+	private static String diagnosisLongCode;
+	
+	public InitialDiagnosis() {
+		super("2.01", "This is a working diagnosis whose primary purpose is to identify those patients with a diagnosis of definite ST elevation MI. "
+				+ "This includes an initial diagnosis made by an ambulance paramedic crew, or other clinician in a position to provide definitive treatment. "
+				+ "Do not change Initial diagnosis on the basis of further ECGs or enzymes/markers. (See 2.03 ECG determining treatment).");
+	}
+	
+	public static void setInitialDiagnosis(byte iniDiagnosis) {
+		// check if valid entry
+		if (iniDiagnosis == 1 || iniDiagnosis == 3 || iniDiagnosis == 4 || iniDiagnosis == 5) {
+			initialDiagnosis = iniDiagnosis;
+			switch (initialDiagnosis) {
+			case 1 : diagnosisLongCode = "Definite myocardial infarction"; break;
+			case 3 : diagnosisLongCode = "Acute coronary syndrome"; /* field 4.32 (High Risk nSTEMI).visible = true; */ break;
+			case 4 : diagnosisLongCode = "Chest pain ? cause"; break;
+			case 5 : diagnosisLongCode = "Other initial diagnosis"; break;
+			}
+		}
+		else
+			System.out.println("Invalid Entry");
+		// set additional fields to visible here
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- minap-desktop-prototype/src/com/team7/minap/datacollection/values/PatientCaseRecordNumber.java
+++ minap-desktop-prototype/src/com/team7/minap/datacollection/values/PatientCaseRecordNumber.java
@@ -1,8 +1,23 @@
+/** Field 1.02 - Patient Case Record Number
+ * Takes in an alphanumeric string that uniquely identifies this particular patient's data.
+ * cNumber must be 10 characters long. This field is encrypted.
+ * @param cNumber
+ * @author david
+ */
 package com.team7.minap.datacollection.values;
 
 public class PatientCaseRecordNumber extends Value {
 	
+	private static String caseNumber; // 1.02
+	private static final short VAL_LENGTH = 10; // Set max length for caseNumber variable
+
 	public PatientCaseRecordNumber() {	
 		super("1.02", "Hospital number is required for identification if the NHS number is not known.");
+	}
+	
+	public static void setCaseNumber(String cNumber) {
+		// length check
+		if (cNumber.length() == VAL_LENGTH)
+		caseNumber = cNumber;
 	}
 }
