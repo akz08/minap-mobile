1b0791d48253641b62da857e04fb3e1944cec49a
daviducl <vo2david@hotmail.com>
2013-12-20 11:05:03 -0700

    SQLite select all seems to be working. Select and update individual need work

--- minap-mobile/src/com/ucl/appteam7/minapmobile/MainActivity.java
+++ minap-mobile/src/com/ucl/appteam7/minapmobile/MainActivity.java
@@ -1,17 +1,64 @@
 package com.ucl.appteam7.minapmobile;
 
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
+import android.database.Cursor;
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		DBAdapter db = new DBAdapter(this);
+		
+		// add a test patient
+//		db.open();
+//		db.insertPatient("Smith", new Date());
+//		db.insertPatient("Jones", new Date());
+		
+		// get all patients
+		db.open();
+		Cursor c = db.allPatients();
+		if (c.moveToFirst())
+		{
+			do {
+				DisplayPatient(c);
+			} while (c.moveToNext());
+		}
+		db.close();
+		
+		// get a patient by last name - currently not working??
+//		db.open();
+//		Cursor c = db.getPatient("Smith");
+//		if (c.moveToFirst())
+//		{
+//			DisplayPatient(c);
+//		}
+//		else 
+//			Toast.makeText(this, "No Contact Found", Toast.LENGTH_LONG).show();
+//		db.close();
+		
+		// update a patient
+//		db.open();
+//		if (db.updatePatient("Smith", new Date()))
+//			Toast.makeText(this, "Update Successful", Toast.LENGTH_LONG).show();
+//		else
+//			Toast.makeText(this, "Update failed", Toast.LENGTH_LONG).show();
+//		db.close();
 	}
-
+	
+	// Display test patients 
+	public void DisplayPatient(Cursor c)
+	{
+		Toast.makeText(this, "Surname: " + c.getString(0) + "\n"
+				+ "DOB: " + c.getString(1), Toast.LENGTH_LONG).show();
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
