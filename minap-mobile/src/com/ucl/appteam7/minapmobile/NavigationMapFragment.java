846716fd58e8c32a817e7c25a077b80d9115dbfa
Amer Kamil Zainal <amer.abidin.13@ucl.ac.uk>
2013-12-26 17:51:01 +0000

    Enabled navigation drawer icon (finally)

--- minap-mobile/src/com/ucl/appteam7/minapmobile/NavigationMapFragment.java
+++ minap-mobile/src/com/ucl/appteam7/minapmobile/NavigationMapFragment.java
@@ -1,24 +1,31 @@
 package com.ucl.appteam7.minapmobile;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class NavigationMapFragment extends Fragment {
 	// stuff for temporary app drawer
+	private static final int DRAWER_OPEN_DELAY = 600;
+	
 	 private String[] mNavigationTitles;
 	 private DrawerLayout mDrawerLayout;
+	 private ActionBarDrawerToggle mDrawerToggle;
 	 private ListView mDrawerList;
 	 private boolean mFirstSession = true;
+	 
 
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -41,7 +48,36 @@ public class NavigationMapFragment extends Fragment {
                 R.layout.drawer_list_item, mNavigationTitles));
         
         // Set the list's click listener
-//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        
+        
+        // link interactions of drawer and action bar icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+        		getActivity(), 
+        		mDrawerLayout,
+        		R.drawable.ic_drawer, 
+        		R.string.drawer_open, 
+        		R.string.drawer_close) {
+        	
+        	// called when drawer completely closed
+        	public void onDrawerClosed(View view) {
+        		
+        	}
+        	// called when drawer completely opened
+        	public void onDrawerOpened(View view) {
+        		
+        	}
+        };
+        
+        // enable app icon to act as action to toggle nav drawer
+        getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActivity().getActionBar().setHomeButtonEnabled(true);
+        getActivity().getActionBar().setDisplayShowHomeEnabled(true);
+        
+        // set drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        
+
 		
 		return view;
 	}
@@ -50,21 +86,61 @@ public class NavigationMapFragment extends Fragment {
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.fragment_navigation_map, menu);
+		
 	}
 	
 	@Override
 	public void onResume() {
 	    super.onResume(); 
+	    mDrawerToggle.syncState();
 	    
 	    // if first session, animate the drawer in
 	    if (mFirstSession) {
-		    mDrawerLayout = (DrawerLayout) getActivity().findViewById(R.id.drawer_layout);
 		    mDrawerLayout.postDelayed(new Runnable() {
 		        @Override
 		        public void run() {
 		            mDrawerLayout.openDrawer(Gravity.LEFT);
 		        }
-		    }, 600);
+		    }, DRAWER_OPEN_DELAY);
+		    
+		    // TODO set mFirstSession to be false as part of the saved bundle arguments 
 	    }
+	}
+	
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+	    super.onConfigurationChanged(newConfig);
+	    mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	
+	@Override 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		};
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView parent, View view, int position, long id) {
+			selectItem(position);
+		}
+	}
+
+	
+	/**
+	 * Called by DrawerItemClickListener when drawer item is clicked
+	 * 
+	 * @param position position of item in drawer's list
+	 */
+	private void selectItem(int position) {
+		// just close the drawer for now
+		mDrawerLayout.closeDrawer(mDrawerList);
 	}
 }
