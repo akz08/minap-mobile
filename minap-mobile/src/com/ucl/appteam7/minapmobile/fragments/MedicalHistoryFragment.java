05463e718d00bf0dd9b93d42410f38365812c6bf
Amer Kamil Zainal <amer.abidin.13@ucl.ac.uk>
2014-01-17 02:02:02 +0000

    Completed "dumb" Medical History page.

--- minap-mobile/src/com/ucl/appteam7/minapmobile/fragments/MedicalHistoryFragment.java
+++ minap-mobile/src/com/ucl/appteam7/minapmobile/fragments/MedicalHistoryFragment.java
@@ -3,12 +3,15 @@ package com.ucl.appteam7.minapmobile.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.ucl.appteam7.minapmobile.R;
@@ -17,6 +20,25 @@ import com.ucl.appteam7.minapmobile.activities.ExaminationsActivity;
 import com.ucl.appteam7.minapmobile.views.MedicalHistoryView;
 
 public class MedicalHistoryFragment extends Fragment {
+	
+	private static final String DIALOG_ABOUT = "about";
+	
+	private Spinner mPreviousAmiSpinner;
+	private Spinner mHypertensionSpinner;
+	private Spinner mCerebroDiseaseSpinner;
+	private Spinner mPreviousPCISpinner;
+	private Spinner mSmokingStatusSpinner;
+	private Spinner mDiabetesSpinner;
+	
+	private Spinner mPreviousAnginaSpinner;
+	private Spinner mHypercholSpinner;
+	private Spinner mAsthmaCopdSpinner;
+	private Spinner mPreviousCabgSpinner;
+	
+	private Spinner mHeartFailureSpinner;
+	private Spinner mPeripheralVascularDiseaseSpinner;
+	private Spinner mChronicRenalFailureSpinner;
+	private Spinner mFamilyHistoryOfChdSpinner;
 	
 	private MedicalHistoryView view;
 	
@@ -31,6 +53,77 @@ public class MedicalHistoryFragment extends Fragment {
 		view = (MedicalHistoryView)View.inflate(getActivity(), R.layout.fragment_medical_history, null);
 		view.setViewListener(viewListener);
 		
+		// Populate spinners
+		mPreviousAmiSpinner = (Spinner)view.findViewById(R.id.previous_ami_spinner);
+		ArrayAdapter<CharSequence> amiAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.previous_ami_array, R.layout.multiline_spinner_item);
+		amiAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mPreviousAmiSpinner.setAdapter(amiAdapter);
+		
+		mHypertensionSpinner = (Spinner)view.findViewById(R.id.hypertension_spinner);
+		ArrayAdapter<CharSequence> hypertensionAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.hypertension_array, R.layout.multiline_spinner_item);
+		hypertensionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mHypertensionSpinner.setAdapter(hypertensionAdapter);
+		
+		mCerebroDiseaseSpinner = (Spinner)view.findViewById(R.id.cerebrovascular_disease_spinner);
+		ArrayAdapter<CharSequence> cerebroAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.cerebrovascular_disease_array, R.layout.multiline_spinner_item);
+		cerebroAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mCerebroDiseaseSpinner.setAdapter(cerebroAdapter);
+		
+		mPreviousPCISpinner = (Spinner)view.findViewById(R.id.previous_pci_spinner);
+		ArrayAdapter<CharSequence> pciAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.previous_pci_array, R.layout.multiline_spinner_item);
+		pciAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mPreviousPCISpinner.setAdapter(pciAdapter);
+		
+		mSmokingStatusSpinner = (Spinner)view.findViewById(R.id.smoking_status_spinner);
+		ArrayAdapter<CharSequence> smokingAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.smoking_status_array, R.layout.multiline_spinner_item);
+		smokingAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mSmokingStatusSpinner.setAdapter(smokingAdapter);
+		
+		mDiabetesSpinner = (Spinner)view.findViewById(R.id.diabetes_spinner);
+		ArrayAdapter<CharSequence> diabetesAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.diabetes_array, R.layout.multiline_spinner_item);
+		diabetesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mDiabetesSpinner.setAdapter(diabetesAdapter);
+		
+		mPreviousAnginaSpinner = (Spinner)view.findViewById(R.id.previous_angina_spinner);
+		ArrayAdapter<CharSequence> anginaAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.previous_angina_array, R.layout.multiline_spinner_item);
+		anginaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mPreviousAnginaSpinner.setAdapter(anginaAdapter);
+		
+		mHypercholSpinner = (Spinner)view.findViewById(R.id.hypercholesterolaemia_spinner);
+		ArrayAdapter<CharSequence> hypercholAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.hypercholesterolaemia_array, R.layout.multiline_spinner_item);
+		hypercholAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mHypercholSpinner.setAdapter(hypercholAdapter);
+		
+		mAsthmaCopdSpinner = (Spinner)view.findViewById(R.id.asthma_copd_spinner);
+		ArrayAdapter<CharSequence> asthmaAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.asthma_copd_array, R.layout.multiline_spinner_item);
+		asthmaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mAsthmaCopdSpinner.setAdapter(asthmaAdapter);
+		
+		mPreviousCabgSpinner = (Spinner)view.findViewById(R.id.previous_cabg_spinner);
+		ArrayAdapter<CharSequence> cabgAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.previous_cabg_array, R.layout.multiline_spinner_item);
+		cabgAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mPreviousCabgSpinner.setAdapter(cabgAdapter);
+		
+		mHeartFailureSpinner = (Spinner)view.findViewById(R.id.heart_failure_spinner);
+		ArrayAdapter<CharSequence> heartAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.heart_failure_array, R.layout.multiline_spinner_item);
+		heartAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mHeartFailureSpinner.setAdapter(heartAdapter);
+		
+		mPeripheralVascularDiseaseSpinner = (Spinner)view.findViewById(R.id.peripheral_vascular_disease_spinner);
+		ArrayAdapter<CharSequence> vascularAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.peripheral_vascular_disease_array, R.layout.multiline_spinner_item);
+		vascularAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mPeripheralVascularDiseaseSpinner.setAdapter(vascularAdapter);
+		
+		mChronicRenalFailureSpinner = (Spinner)view.findViewById(R.id.chronic_renal_failure_spinner);
+		ArrayAdapter<CharSequence> renalAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.chronic_renal_failure_array, R.layout.multiline_spinner_item);
+		renalAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mChronicRenalFailureSpinner.setAdapter(renalAdapter);
+		
+		mFamilyHistoryOfChdSpinner = (Spinner)view.findViewById(R.id.family_history_of_chd_spinner);
+		ArrayAdapter<CharSequence> chdAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.family_history_of_chd_array, R.layout.multiline_spinner_item);
+		chdAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		mFamilyHistoryOfChdSpinner.setAdapter(chdAdapter);
+		
 		// setup action bar 
 		getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -38,6 +131,160 @@ public class MedicalHistoryFragment extends Fragment {
 	}
 	
 	private MedicalHistoryView.ViewListener viewListener = new MedicalHistoryView.ViewListener() {
+
+		@Override
+		public void showAboutPreviousAmi() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutHypertension() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutCerebroDisease() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutPreviousPCI() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutSmokingStatus() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutDiabetes() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutPreviousAngina() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutHyperchol() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutAsthmaCopd() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutPreviousCabg() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutHeartFailure() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutPeripheralVascularDisease() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutChronicRenalFailure() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
+
+		@Override
+		public void showAboutFamilyHistoryOfChd() {
+			// TODO: Replace title and content with calls to the model
+			FragmentManager fm = getActivity()
+					.getSupportFragmentManager();
+			AboutDialogFragment dialog = AboutDialogFragment
+					.newInstance("Dummy Title", "dummy content");
+			dialog.show(fm, DIALOG_ABOUT);			
+			
+		}
 		
 		@Override
 		public void previousPage() {
--- minap-mobile/src/com/ucl/appteam7/minapmobile/views/MedicalHistoryView.java
+++ minap-mobile/src/com/ucl/appteam7/minapmobile/views/MedicalHistoryView.java
@@ -6,15 +6,52 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ScrollView;
 
 public class MedicalHistoryView extends ScrollView {
 
+	private ImageButton mAboutPreviousAmiButton;
+	private ImageButton mAboutHypertensionButton;
+	private ImageButton mAboutCerebroDiseaseButton;
+	private ImageButton mAboutPreviousPCIButton;
+	private ImageButton mAboutSmokingStatusButton;
+	private ImageButton mAboutDiabetesButton;
+	
+	private ImageButton mAboutPreviousAnginaButton;
+	private ImageButton mAboutHypercholButton;
+	private ImageButton mAboutAsthmaCopdButton;
+	private ImageButton mAboutPreviousCabgButton;
+	
+	private ImageButton mAboutHeartFailureButton;
+	private ImageButton mAboutPeripheralVascularDiseaseButton;
+	private ImageButton mAboutChronicRenalFailureButton;
+	private ImageButton mAboutFamilyHistoryOfChdButton;
+	
 	private Button mPreviousPage;
 	private Button mNextPage;
 	
 	// Define methods that the fragment needs to implement
 	public static interface ViewListener {
+		
+		// about dialogs
+		public void showAboutPreviousAmi();
+		public void showAboutHypertension();
+		public void showAboutCerebroDisease();
+		public void showAboutPreviousPCI();
+		public void showAboutSmokingStatus();
+		public void showAboutDiabetes();
+		
+		public void showAboutPreviousAngina();
+		public void showAboutHyperchol();
+		public void showAboutAsthmaCopd();
+		public void showAboutPreviousCabg();
+		
+		public void showAboutHeartFailure();
+		public void showAboutPeripheralVascularDisease();
+		public void showAboutChronicRenalFailure();
+		public void showAboutFamilyHistoryOfChd();
+		
 		// navigation buttons
 		public void previousPage();
 		public void nextPage();
@@ -34,6 +71,147 @@ public class MedicalHistoryView extends ScrollView {
 	@Override
 	protected void onFinishInflate() {
 		super.onFinishInflate();
+		
+		// wire up 'about' information buttons
+		mAboutPreviousAmiButton = (ImageButton)findViewById(R.id.about_previous_ami);
+		mAboutPreviousAmiButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutPreviousAmi();
+				
+			}
+		});
+		
+		mAboutHypertensionButton = (ImageButton)findViewById(R.id.about_hypertension);
+		mAboutHypertensionButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutHypertension();
+				
+			}
+		});
+		
+		mAboutCerebroDiseaseButton = (ImageButton)findViewById(R.id.about_cerebrovascular_disease);
+		mAboutCerebroDiseaseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutCerebroDisease();
+				
+			}
+		});
+		
+		mAboutPreviousPCIButton = (ImageButton)findViewById(R.id.about_previous_pci);
+		mAboutPreviousPCIButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutPreviousPCI();
+				
+			}
+		});
+		
+		mAboutSmokingStatusButton = (ImageButton)findViewById(R.id.about_smoking_status);
+		mAboutSmokingStatusButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutSmokingStatus();
+				
+			}
+		});
+		
+		mAboutDiabetesButton = (ImageButton)findViewById(R.id.about_diabetes);
+		mAboutDiabetesButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutDiabetes();
+				
+			}
+		});
+		
+		mAboutPreviousAnginaButton = (ImageButton)findViewById(R.id.about_previous_angina);
+		mAboutPreviousAnginaButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutPreviousAngina();
+				
+			}
+		});
+		
+		mAboutHypercholButton = (ImageButton)findViewById(R.id.about_hypercholesterolaemia);
+		mAboutHypercholButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutHyperchol();
+				
+			}
+		});
+		
+		mAboutAsthmaCopdButton = (ImageButton)findViewById(R.id.about_asthma_copd);
+		mAboutAsthmaCopdButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutAsthmaCopd();
+				
+			}
+		});
+		
+		mAboutPreviousCabgButton = (ImageButton)findViewById(R.id.about_previous_cabg);
+		mAboutPreviousCabgButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutPreviousCabg();
+				
+			}
+		});
+		
+		mAboutHeartFailureButton = (ImageButton)findViewById(R.id.about_heart_failure);
+		mAboutHeartFailureButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutHeartFailure();
+				
+			}
+		});
+		
+		mAboutPeripheralVascularDiseaseButton = (ImageButton)findViewById(R.id.about_peripheral_vascular_disease);
+		mAboutPeripheralVascularDiseaseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutPeripheralVascularDisease();
+				
+			}
+		});
+		
+		mAboutChronicRenalFailureButton = (ImageButton)findViewById(R.id.about_chronic_renal_failure);
+		mAboutChronicRenalFailureButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutChronicRenalFailure();
+				
+			}
+		});
+		
+		mAboutFamilyHistoryOfChdButton = (ImageButton)findViewById(R.id.about_family_history_of_chd);
+		mAboutFamilyHistoryOfChdButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				viewListener.showAboutFamilyHistoryOfChd();
+				
+			}
+		});
 		
 		// wire up page navigation buttons
 		mPreviousPage = (Button)findViewById(R.id.previous_page_button);
