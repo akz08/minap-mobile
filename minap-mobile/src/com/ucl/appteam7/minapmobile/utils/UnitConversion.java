a64159704d0029746d9291e8efb61ea9cd6cd285
Amer Kamil Zainal <amer.abidin.13@ucl.ac.uk>
2014-01-18 20:25:47 +0000

    Added shell unit conversion class. Needs to be implemented.

---
+++ minap-mobile/src/com/ucl/appteam7/minapmobile/utils/UnitConversion.java
@@ -0,0 +1,33 @@
+/**
+ * A unit conversion class to be used primarily for unit conversion in the Examinations section for BMI
+ * 
+ */
+
+package com.ucl.appteam7.minapmobile.utils;
+
+public class UnitConversion {
+
+	public static int stonesToKg(int stones) {
+		return stonesAndPoundsToKg(stones, 0);
+	}
+	
+	public static int poundsToKg(int pounds) {
+		return stonesAndPoundsToKg(0, pounds);
+	}
+	
+	public static int stonesAndPoundsToKg(int stones, int pounds) {
+		return 0;
+	}
+	
+	public static int feetToCms(int feet) {
+		return feetAndInchesToCms(feet, 0);
+	}
+	
+	public static int inchesToCms(int inches) {
+		return feetAndInchesToCms(0, inches);
+	}
+	
+	public static int feetAndInchesToCms(int feet, int inches) {
+		return 0;
+	}
+}
