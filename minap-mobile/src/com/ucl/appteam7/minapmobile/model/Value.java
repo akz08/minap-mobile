4c24b4ec7a31e334d35eefee3893f8841315611d
Amer Kamil Zainal <amer.abidin.13@ucl.ac.uk>
2014-01-18 11:50:04 +0000

    Added notesTitle String to Value.java with a cascading constructor to keep things compiling. Using an obviously wrong title string as the default to help debug.

--- minap-mobile/src/com/ucl/appteam7/minapmobile/model/Value.java
+++ minap-mobile/src/com/ucl/appteam7/minapmobile/model/Value.java
@@ -1,27 +1,42 @@
 package com.ucl.appteam7.minapmobile.model;
 //All values extents this, every page will check these methods. 
 public class Value {
-	public Value (String id, String notes) {
-		this.id = id; 
-		this.notes = notes;
-	}
+	
+	private String id;			// MINAP value id
+	private String notes;		// contents of help notes  (this is not stored on the local database)
+	private String notesTitle;	// the title of help notes (this is not stored on the local database)
+	
 	private boolean isValid;
 	private boolean isComplete;
+	
+	public Value (String id, String notes) {
+		this(id, notes, "Title to be Added"); // use an obviously wrong default title - all help dialogs must have titles.
+	}
+	
+	public Value (String id, String notes, String notesTitle) {
+		this.id = id;
+		this.notes = notes;
+		this.notesTitle = notesTitle;
+	}
+	
 	public boolean isValidated() {
 		return isValid;
 	}
+	
 	public boolean isCompleted() {
 		return isComplete;
 	}
-	private String id;
+	
 	public String getId (){
 		return id;
-	}
-	private String notes;
+	}	
 	
 	public String getNotes (){
 		return notes;
 	}
 	
+	public String getNotesTitle (){
+		return notesTitle;
+	}
 
 }
